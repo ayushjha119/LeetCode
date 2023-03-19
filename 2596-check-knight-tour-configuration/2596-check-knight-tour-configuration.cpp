79d991154044d9d20ebd02da2e2590e76f78c7c2
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2023-03-19 12:14:05 +0530

    Time: 14 ms (66.67%), Space: 27.9 MB (33.33%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,17 +1,40 @@
 class Solution {
-    bool solve(vector<vector<int>>& grid, int i, int j, int& n, int t)
-    {
-        if(i < 0 || i >= n || j < 0 || j >= n) return false;
-        if(grid[i][j] != t) return false;
-        if(grid[i][j] == n*n-1 && t == n*n-1) return true;
-        return solve(grid, i+2, j+1, n, t+1) || solve(grid, i+2, j-1, n, t+1) ||
-                solve(grid, i-2, j+1, n, t+1) || solve(grid, i-2, j-1, n, t+1) ||
-                solve(grid, i+1, j+2, n, t+1) || solve(grid, i+1, j-2, n, t+1) ||
-                solve(grid, i-1, j+2, n, t+1) || solve(grid, i-1, j-2, n, t+1);
-    }
 public:
     bool checkValidGrid(vector<vector<int>>& grid) {
-        int n = grid.size();
-        return solve(grid, 0, 0, n, 0);
+        
+        // let us define the direction in which knight can travel
+        
+        int dir[8][2] = {{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2}};
+        
+         int count =0, maxCount = grid.size()*grid.size()-1;
+        
+        int x = 0,y = 0, i ,j;
+        
+        if(grid[x][y]!=count) return false;
+        
+        while(count<maxCount){
+            //count ko 1 increse kiye aur 0  sey 1 ho gya aur 
+            //phir harr direction mai  check kiye ki  1 kaha kaha ho skta hai 
+            
+            count++;
+            int flag  =1;
+            for(auto d : dir)
+            {
+                i = x+d[0];
+                j = y+d[1];
+                
+                if(i>=0 && j>=0 && i<grid.size() && j<grid.size() && grid[i][j]==count){
+                    x= i;y=j;flag=0;break;
+                }
+            }
+            
+            // kisi ek ko v place nahi kar paye toh wahi return false aur exit
+            if(flag)
+                return false;
+            
+        }
+        
+        return true;
+        
     }
 };
