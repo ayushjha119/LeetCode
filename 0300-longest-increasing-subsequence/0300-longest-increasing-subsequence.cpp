8e4a953e0ff30b4231fae1e1d2d111ecaca690ab
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2024-01-05 17:36:40 +0530

    Time: 497 ms (27.38%), Space: 292.8 MB (6.53%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(int ind,int prev_idx,vector<int> &nums,int n,vector<vector<int>> &dp){
+        if(ind==n)
+            return 0;
+        if(dp[ind][prev_idx+1]!=-1)
+            return dp[ind][prev_idx];
+        int len = 0+ f(ind+1,prev_idx,nums,n,dp);
+        
+        if(prev_idx==-1 || nums[ind]>nums[prev_idx]){
+            len = max(len,1+f(ind+1,ind,nums,n,dp));
+        }
+        
+        return len;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind = n-1;ind>=0;ind--){
+            for(int prev_ind = ind-1;prev_ind>=-1;prev_ind--){
+                int len = 0+dp[ind+1][prev_ind+1];
+                
+                 if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
+                    len = max(len,1+dp[ind+1][ind+1]);
+                }
+                dp[ind][prev_ind+1]=len;
+            }
+            
+            
+            
+        }
+      return dp[0][-1+1];
+    }
+};
