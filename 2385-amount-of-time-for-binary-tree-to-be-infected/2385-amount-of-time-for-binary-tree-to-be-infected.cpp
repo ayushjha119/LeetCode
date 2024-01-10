be05feb7787c936b1b5360100beadf7d014883ea
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2024-01-10 12:40:59 +0530

    Time: 134 ms (94.38%), Space: 90.4 MB (100%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,17 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-       int amountOfTime(TreeNode* root, int start) {
+
+/*
+ int amountOfTime(TreeNode* root, int start) {
         unordered_map<int, unordered_set<int>> map;
         convert(root, 0, map);
         queue<int> q;
@@ -59,4 +48,50 @@ public:
         convert(current->left, current->val, map);
         convert(current->right, current->val, map);
     }
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int maxDistance = 0;
+
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        traverse(root, start);
+        return maxDistance;
+    }
+
+    int traverse(TreeNode* root, int start) {
+        int depth = 0;
+        if (root == nullptr) {
+            return depth;
+        }
+
+        int leftDepth = traverse(root->left, start);
+        int rightDepth = traverse(root->right, start);
+
+        if (root->val == start) {
+            maxDistance = max(leftDepth, rightDepth);
+            depth = -1;
+        } else if (leftDepth >= 0 && rightDepth >= 0) {
+            depth = max(leftDepth, rightDepth) + 1;
+        } else {
+            int distance = abs(leftDepth) + abs(rightDepth);
+            maxDistance = max(maxDistance, distance);
+            depth = min(leftDepth, rightDepth) - 1;
+        }
+
+        return depth;
+    }
+      
 };
